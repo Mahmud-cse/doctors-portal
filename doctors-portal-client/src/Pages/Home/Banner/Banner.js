9e2a82354513d0adca62df457ac8e9f9844963c3
Mahmud-cse <mahmudhasan555666@gmail.com>
2021-11-03 02:33:32 +0600

    banner

--- doctors-portal-client/src/Pages/Home/Banner/Banner.js
+++ doctors-portal-client/src/Pages/Home/Banner/Banner.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid'
 import chair from '../../../images/chair.png';
 import bg from '../../../images/bg.png';
-import { Button, Typography } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
+const bannerBg = {
+    background: `url(${bg})`,
+}
+
+const verticalCenter = {
+    display: 'flex',
+    height: 450,
+    alignItems: 'center'
+}
 
 const Banner = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
+        <Container style={bannerBg} sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
-                <Grid item xs={12} md={5}>
-                    <Typography variant="h3">
-                        Your new smile <br />
-                        Starts Here
-                    </Typography>
-                    <Typography variant="h6">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nam facere laudantium eligendi ipsa molestiae! Illo animi odio distinctio unde?
-                    </Typography>
-                    <Button variant="contained" style={{ backgroundColor: "#5CE7ED", color: 'black' }}>Get Appointment</Button>
+                <Grid style={{ ...verticalCenter, textAlign: "left" }} item xs={12} md={6}>
+                    <Box>
+                        <Typography variant="h3">
+                            Your new smile <br />
+                            Starts Here
+                        </Typography>
+                        <Typography variant="h6" sx={{ my: 3, color: "gray", fontSize: 14, fontWeight: 300 }}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nam facere laudantium eligendi ipsa molestiae! Illo animi odio distinctio unde?
+                        </Typography>
+                        <Button variant="contained" style={{ backgroundColor: "#5CE7ED", color: 'black' }}>Get Appointment</Button>
+                    </Box>
                 </Grid>
-                <Grid item xs={12} md={7}>
-                    <img src={chair} alt="" />
+                <Grid item xs={12} md={6} style={verticalCenter}>
+                    <img style={{ width: '350px' }} src={chair} alt="" />
                 </Grid>
             </Grid>
-        </Box>
+        </Container>
     );
 };
 
