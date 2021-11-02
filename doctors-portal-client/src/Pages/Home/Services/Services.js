a6cc45b1878cd5acbc8921e4c02ae46ad8f84873
Mahmud-cse <mahmudhasan555666@gmail.com>
2021-11-02 18:15:31 +0600

    service

--- doctors-portal-client/src/Pages/Home/Services/Services.js
+++ doctors-portal-client/src/Pages/Home/Services/Services.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { experimentalStyled as styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Container } from '@mui/material';
@@ -7,6 +6,8 @@ import Service from '../Service/Service';
 import fluoride from '../../../images/fluoride.png';
 import cavity from '../../../images/cavity.png';
 import whitening from '../../../images/whitening.png';
+import Typography from '@mui/material/Typography';
+
 
 const services = [
     {
@@ -30,6 +31,12 @@ const Services = () => {
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Container>
+                <Typography sx={{ fontWeight: 500, m: 2, color: 'info.main' }} variant="h6" component="div">
+                    OUR SERVICES
+                </Typography>
+                <Typography sx={{ fontWeight: 500, m: 5 }} variant="h4" component="div">
+                    Services We Provide
+                </Typography>
                 <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                     {
                         services.map(service => <Service key={service.name} service={service} />)
