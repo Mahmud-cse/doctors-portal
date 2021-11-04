e9883de0aca73350ae2612ff82f296f7b3c567dd
Mahmud-cse <mahmudhasan555666@gmail.com>
2021-11-04 16:12:05 +0600

    booking

--- doctors-portal-client/src/Pages/Appointment/Booking/Booking.js
+++ doctors-portal-client/src/Pages/Appointment/Booking/Booking.js
@@ -1,23 +1,35 @@
 import { Button, Grid, Paper, Typography } from '@mui/material';
 import React from 'react';
+import BookingModal from '../BookingModal/BookingModal';
 
 const Booking = ({ booking }) => {
     const { name, time, space } = booking;
+    const [openBooking, setBookingOpen] = React.useState(false);
+    const handleBookingOpen = () => setBookingOpen(true);
+    const handleBookingClose = () => setBookingOpen(false);
     return (
-        <Grid item xs={12} sm={6} md={4}>
-            <Paper elevation={3} sx={{ py: 5 }}>
-                <Typography style={{ color: "#04AAED", fontWeight: 600 }} variant="h5" gutterBottom component="div">
-                    {name}
-                </Typography>
-                <Typography variant="h6" gutterBottom component="div">
-                    {time}
-                </Typography>
-                <Typography variant="caption" display="block" gutterBottom>
-                    {space} SPACES AVAILABLE
-                </Typography>
-                <Button variant="contained" style={{ backgroundColor: "#5CE7ED", color: 'black' }}>BOOK APPOINTMENT</Button>
-            </Paper>
-        </Grid>
+        <>
+            <Grid item xs={12} sm={6} md={4}>
+                <Paper elevation={3} sx={{ py: 5 }}>
+                    <Typography style={{ color: "#04AAED", fontWeight: 600 }} variant="h5" gutterBottom component="div">
+                        {name}
+                    </Typography>
+                    <Typography variant="h6" gutterBottom component="div">
+                        {time}
+                    </Typography>
+                    <Typography variant="caption" display="block" gutterBottom>
+                        {space} SPACES AVAILABLE
+                    </Typography>
+                    <Button onClick={handleBookingOpen} variant="contained" style={{ backgroundColor: "#5CE7ED", color: 'black' }}>BOOK APPOINTMENT</Button>
+                </Paper>
+            </Grid>
+            <BookingModal
+                booking={booking}
+                openBooking={openBooking}
+                handleBookingClose={handleBookingClose}
+            >
+            </BookingModal>
+        </>
     );
 };
 
